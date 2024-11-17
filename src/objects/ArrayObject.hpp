5c3349fdd54df3d5b849bb0a76953ed543deaf65
Edward Palmer <edward.palmer@ukaea.uk>
2024-11-17 17:44:22 +0000

    Moves ArrayObject into separate file.

---
+++ src/objects/ArrayObject.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file ArrayObject.hpp
+ * @author Edward Palmer
+ * @date 2024-11-17
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+#include "BaseObject.hpp"
+#include <vector>
+
+struct ArrayObject : public BaseObject
+{
+public:
+    ArrayObject() = default;
+    ArrayObject(std::vector<BaseObject *> values_) : values(std::move(values_)) {}
+
+    // TODO: - who owns the objects in the array? MEMORY LEAK!!
+    /// Performs a deep copy of array. This will enable the array to be returned
+    /// by a function without objects (defined in function scope) being destroyed.
+    ArrayObject *clone() const override
+    {
+        std::vector<BaseObject *> cloneValues(values.size());
+
+        for (BaseObject *obj : values)
+        {
+            cloneValues.push_back(obj->clone());
+        }
+
+        return new ArrayObject(cloneValues);
+    }
+
+    std::string typeName() const override { return "ArrayObject"; }
+
+    // TODO: - eventually just store references to BaseObject & or pointers and return reference.
+    BaseObject *operator[](std::size_t index) const
+    {
+        assert(index < values.size());
+        return (values.at(index)); // TODO: - fix this. Just return a reference or something.
+    }
+
+    std::vector<BaseObject *> values;
+};
--- src/objects/EucleiaObject.hpp
+++ src/objects/EucleiaObject.hpp
@@ -8,6 +8,7 @@
 #ifndef EucleiaObject_hpp
 #define EucleiaObject_hpp
 
+#include "ArrayObject.hpp"
 #include "BaseObject.hpp"
 #include "EucleiaUtility.hpp"
 #include "FloatObject.hpp"
@@ -27,40 +28,6 @@ class Scope;
 
 // Forwards declaration.
 class FunctionNode;
-
-
-struct ArrayObject : public BaseObject
-{
-public:
-    ArrayObject() = default;
-    ArrayObject(std::vector<BaseObject *> values_) : values(std::move(values_)) {}
-
-    // TODO: - who owns the objects in the array? MEMORY LEAK!!
-    /// Performs a deep copy of array. This will enable the array to be returned
-    /// by a function without objects (defined in function scope) being destroyed.
-    ArrayObject *clone() const override
-    {
-        std::vector<BaseObject *> cloneValues(values.size());
-
-        for (BaseObject *obj : values)
-        {
-            cloneValues.push_back(obj->clone());
-        }
-
-        return new ArrayObject(cloneValues);
-    }
-
-    std::string typeName() const override { return "ArrayObject"; }
-
-    // TODO: - eventually just store references to BaseObject & or pointers and return reference.
-    BaseObject *operator[](std::size_t index) const
-    {
-        assert(index < values.size());
-        return (values.at(index)); // TODO: - fix this. Just return a reference or something.
-    }
-
-    std::vector<BaseObject *> values;
-};
 
 
 /// FunctionObject contains a pointer to the original function definition which
