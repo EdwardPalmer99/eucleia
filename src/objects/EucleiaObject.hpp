f10c2f6f179734683c26fd93ae5b86e2f9abf843
Edward Palmer <edward.palmer@ukaea.uk>
2024-11-17 17:50:43 +0000

    Moves LibraryFunctionObject out.

--- src/objects/EucleiaObject.hpp
+++ src/objects/EucleiaObject.hpp
@@ -5,49 +5,16 @@
 //  Created by Edward on 14/01/2024.
 //
 
-#ifndef EucleiaObject_hpp
-#define EucleiaObject_hpp
-
+#pragma once
 #include "ArrayObject.hpp"
 #include "BaseObject.hpp"
 #include "EucleiaUtility.hpp"
 #include "FloatObject.hpp"
 #include "FunctionObject.hpp"
 #include "IntObject.hpp"
+#include "LibraryFunctionObject.hpp"
 #include "StringObject.hpp"
-#include <assert.h>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <memory>
 #include <ostream>
-#include <string>
-#include <vector>
-
-class ProgramNode;
-class Scope;
-
-
-/// Library function allows us to define lambdas which wrap around existing stdlib
-/// functions. These can then be added to a global scope after seeing "import <...>"
-/// with angled-brackets.
-struct LibraryFunctionObject : public BaseObject
-{
-public:
-    using LibraryFunction = std::function<BaseObject *(ProgramNode &, Scope &)>;
-
-    LibraryFunctionObject(LibraryFunction function_) : evaluate(std::move(function_)) {}
-
-    std::string typeName() const override { return "LibraryFunctionObject"; }
-
-    LibraryFunctionObject *clone() const override
-    {
-        return new LibraryFunctionObject(evaluate);
-    }
-
-    LibraryFunction evaluate;
-};
-
 
 inline std::ostream &operator<<(std::ostream &out, const BaseObject &object)
 {
@@ -72,5 +39,3 @@ inline std::ostream &operator<<(std::ostream &out, const BaseObject &object)
     else
         return out; // Don't print anything.
 }
-
-#endif /* EucleiaObject_hpp */
