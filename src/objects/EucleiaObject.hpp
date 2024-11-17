862903d867548b69f7650ae18963e90f2562ca7a
Edward Palmer <edward.palmer@ukaea.uk>
2024-11-17 17:36:46 +0000

    Moves BaseObject into separate file.

--- src/objects/EucleiaObject.hpp
+++ src/objects/EucleiaObject.hpp
@@ -8,6 +8,7 @@
 #ifndef EucleiaObject_hpp
 #define EucleiaObject_hpp
 
+#include "BaseObject.hpp"
 #include "EucleiaUtility.hpp"
 #include <assert.h>
 #include <functional>
@@ -24,49 +25,6 @@ class Scope;
 // Forwards declaration.
 class FunctionNode;
 class FloatObject;
-
-/**
- * BaseObject. All objects are derived from this class.
- */
-class BaseObject
-{
-public:
-    virtual ~BaseObject() = default;
-
-    /// Cast to object type.
-    template <class TObject>
-    const TObject &castObject() const
-    {
-        return static_cast<const TObject &>(*this);
-    }
-
-    template <class TObject>
-    TObject &castObject()
-    {
-        return static_cast<TObject &>(*this);
-    }
-
-    /// Check object type.
-    template <class TObject>
-    bool isObjectType() const
-    {
-        return typeid(*this) == typeid(TObject);
-    }
-
-    bool typesMatch(const BaseObject &other) const
-    {
-        return typeid(*this) == typeid(other);
-    }
-
-    // TODO: - clone should be called with a scope specified to handle memory.
-    /// Implement creating a copy for derived classes.
-    virtual BaseObject *clone() const = 0;
-
-    virtual std::string typeName() const = 0;
-
-protected:
-    BaseObject() = default;
-};
 
 /**
  * @brief Long integer type.
