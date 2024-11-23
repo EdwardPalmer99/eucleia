e4765a1198e1195ca61e3b08f4f763aed485a471
Edward Palmer <edward.palmer@ukaea.uk>
2024-11-23 00:23:37 +0000

    Fixes segmentation fault.

---
+++ src/utility/JumpPoints.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file JumpPoints.cpp
+ * @author Edward Palmer
+ * @date 2024-11-23
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "JumpPoints.hpp"
+
+GlobalEnvRec gEnvironmentContext;
