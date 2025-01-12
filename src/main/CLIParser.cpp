993f26d491d342247c9b3b4060166c3d3e5956c5
Edward Palmer <{username}@users.noreply.github.com>
2025-01-12 21:39:47 +0000

    Adds CLIParser class.

---
+++ src/main/CLIParser.cpp
@@ -0,0 +1,134 @@
+/**
+ * @file CLIParser.cpp
+ * @author Edward Palmer
+ * @date 2025-01-12
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "CLIParser.hpp"
+#include "Exceptions.hpp"
+#include <stdexcept>
+
+CLIParser::CLIParser(std::string programName_)
+    : programName(std::move(programName_))
+{
+}
+
+void CLIParser::addPositionalArg(std::string name)
+{
+    if (isPositionalArg(name))
+        ThrowException(name + " is already defined");
+
+    cliPositionalArgs[name] = std::nullopt;
+}
+
+void CLIParser::addFlagArg(std::string name, std::optional<std::string> description)
+{
+    if (!hasDashPrefix(name))
+        ThrowException("incorrect format for flag argument");
+    if (isFlag(name))
+        ThrowException("argument " + name + " is already defined");
+
+    cliFlags[name] = {.state = false, .description = std::move(description)};
+}
+
+const std::string &CLIParser::operator[](const std::string &name) const
+{
+    if (!isPositionalArg(name))
+        ThrowException("unknown argument: " + name);
+
+    auto &optional = cliPositionalArgs.at(name);
+    if (!optional)
+        ThrowException("positional argument " + name + " is null");
+
+    return *optional;
+}
+
+bool CLIParser::isSet(const std::string &name) const
+{
+    if (isFlag(name))
+        return (cliFlags.at(name).state);
+    else if (isPositionalArg(name))
+        return (cliPositionalArgs.at(name) != std::nullopt);
+    else
+        ThrowException("unknown argument: " + name);
+}
+
+void CLIParser::setFlag(const std::string &name, bool state)
+{
+    if (!isFlag(name))
+        ThrowException("unrecognized flag: " + name);
+
+    cliFlags.at(name).state = state;
+}
+
+void CLIParser::setNextPositionalArg(const std::string &value)
+{
+    for (auto &iter : cliPositionalArgs) // Set in order provided.
+    {
+        if (!iter.second)
+        {
+            (*iter.second) = value;
+            return;
+        }
+    }
+
+    // Uh-oh!
+    ThrowException("too many positional arguments supplied");
+}
+
+void CLIParser::parseArgs(int argc, const char *argv[])
+{
+    for (int iarg = 1; iarg < argc; ++iarg)
+    {
+        std::string value{argv[iarg]};
+
+        if (hasDashPrefix(value))
+            setFlag(value);
+        else
+            setNextPositionalArg(value);
+    }
+}
+
+void CLIParser::printOptions(std::ostream &out) const
+{
+    out << "USAGE: " << programName;
+
+    for (auto &[name, _] : cliPositionalArgs)
+    {
+        out << " [" << name << "]";
+    }
+
+    out << " [options]" << std::endl;
+
+    const size_t kBufferSize = 250;
+    char lineBuffer[kBufferSize];
+
+    out << "OPTIONS:" << std::endl;
+    for (auto &[name, flagInfo] : cliFlags)
+    {
+        if (flagInfo.description)
+            snprintf(lineBuffer, kBufferSize, "\t%-20s %s", name.c_str(), (*flagInfo.description).c_str());
+        else
+            snprintf(lineBuffer, kBufferSize, "\t%-20s", name.c_str());
+
+        out << std::string(lineBuffer) << std::endl;
+    }
+}
+
+bool CLIParser::hasDashPrefix(const std::string &name) const
+{
+    return (name.compare(0, 2, "--", 0, 2) == 0);
+}
+
+bool CLIParser::isFlag(const std::string &name) const
+{
+    return cliFlags.count(name);
+}
+
+bool CLIParser::isPositionalArg(const std::string &name) const
+{
+    return cliPositionalArgs.count(name);
+}
---
+++ src/main/CLIParser.hpp
@@ -0,0 +1,73 @@
+/**
+ * @file CLIParser.hpp
+ * @author Edward Palmer
+ * @date 2025-01-12
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#pragma once
+#include "Exceptions.hpp"
+#include <iostream>
+#include <map>
+#include <optional>
+#include <ostream>
+#include <string>
+#include <unordered_map>
+
+class CLIParser
+{
+public:
+    CLIParser() = delete;
+    CLIParser(std::string programName);
+
+    // Add a required positional argument.
+    void addPositionalArg(std::string argName);
+
+    // Add a new flag (on/off).
+    void addFlagArg(std::string flagName, std::optional<std::string> description = std::nullopt);
+
+    // Parse CLI arguments from main().
+    void parseArgs(int argc, const char *argv[]);
+
+    // Returns true if flag/positional argument is set.
+    bool isSet(const std::string &name) const;
+
+    // Returns reference to positional argument's value.
+    const std::string &operator[](const std::string &name) const;
+
+    // Prints available options.
+    void printOptions(std::ostream &out = std::cout) const;
+
+protected:
+    // Sets a flag.
+    void setFlag(const std::string &name, bool state = true);
+
+    // Sets the next positional argument.
+    void setNextPositionalArg(const std::string &value);
+
+    // Returns true if this is a CLI flag.
+    bool isFlag(const std::string &name) const;
+
+    // Returns true if this is a positional argument.
+    bool isPositionalArg(const std::string &name) const;
+
+    // Checks flag name has '--' dashes.
+    bool hasDashPrefix(const std::string &name) const;
+
+private:
+    const std::string programName;
+
+    // Map of positional arguments.
+    std::map<std::string, std::optional<std::string>> cliPositionalArgs;
+
+    struct FlagRec
+    {
+        bool state;
+        std::optional<std::string> description;
+    };
+
+    // Map of cli flags. Default to off (false).
+    std::unordered_map<std::string, FlagRec> cliFlags;
+};
