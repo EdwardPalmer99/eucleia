c80af378c284a91563801e2c257e63bcc557312d
Edward Palmer <{username}@users.noreply.github.com>
2025-04-16 12:58:26 +0100

    Adds Bazel build files

---
+++ test/DummyTest.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file testme.cpp
+ * @author Edward Palmer
+ * @date 2025-04-16
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "Scope.hpp"
+#include <gtest/gtest.h>
+
+TEST(Dummy, BasicTest)
+{
+    EXPECT_EQ(1, 1);
+}
