3f6ebb16024afe67edc9ddacf58cd3d4a5523304
Edward Palmer <{username}@users.noreply.github.com>
2025-05-08 00:17:51 +0100

    Adds NodeType enum

---
+++ src/nodes/NodeType.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file NodeType.hpp
+ * @author Edward Palmer
+ * @date 2025-05-08
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+
+#pragma once
+
+enum class NodeType
+{
+    Unknown = (-1),
+    Bool,
+    Int,
+    String,
+    Float,
+    If,
+};
