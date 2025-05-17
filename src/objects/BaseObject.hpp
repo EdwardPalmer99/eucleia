9c0aae7dc25174993f6c0e8b1b90ac9e0ddc7325
Edward Palmer <{username}@users.noreply.github.com>
2025-05-17 14:47:38 +0100

    Removes typesMatch and adds nodiscard

--- src/objects/BaseObject.hpp
+++ src/objects/BaseObject.hpp
@@ -32,32 +32,25 @@ public:
 
     /// Cast to object type.
     template <class TObject>
-    const TObject &castObject() const
+    [[nodiscard]] const TObject &castObject() const
     {
         return static_cast<const TObject &>(*this);
     }
 
     template <class TObject>
-    TObject &castObject()
+    [[nodiscard]] TObject &castObject()
     {
         return static_cast<TObject &>(*this);
     }
 
     /// Check object type.
     template <class TObject>
-    bool isObjectType() const
+    [[nodiscard]] bool isObjectType() const
     {
         return typeid(*this) == typeid(TObject);
     }
 
-    bool typesMatch(const BaseObject &other) const
-    {
-        return typeid(*this) == typeid(other);
-    }
-
-    // TODO: - clone should be called with a scope specified to handle memory.
-    /// Implement creating a copy for derived classes.
-    virtual BaseObject::Ptr clone() const = 0;
+    [[nodiscard]] virtual BaseObject::Ptr clone() const = 0;
 
 protected:
     BaseObject() = default;
