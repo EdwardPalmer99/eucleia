04e46299f7988d0cb0b02bf1ee640efdf8fde4da
Edward Palmer <{username}@users.noreply.github.com>
2025-05-18 14:48:04 +0100

    Reorganised testing

---
+++ test/benchmark/CountTo1MBenchmark.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file CountTo1MBenchmark.cpp
+ * @author Edward Palmer
+ * @date 2025-05-18
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "EucleiaInterpreter.hpp"
+#include "FileParser.hpp"
+#include "Scope.hpp"
+#include "test/utility/Utility.hpp"
+#include <benchmark/benchmark.h>
+
+
+static void BenchmarkParsingCountTo1M(benchmark::State &state)
+{
+    auto path = (getTestDirPath() + "benchmark/data/CountToOneMillion.ek");
+
+    for (auto _ : state)
+    {
+        (void)FileParser::parseMainFile(path);
+    }
+}
+
+
+static void BenchmarkEvaluatingCountTo1M(benchmark::State &state)
+{
+    auto path = (getTestDirPath() + "benchmark/data/CountToOneMillion.ek");
+
+    auto ast = FileParser::parseMainFile(path);
+
+    for (auto _ : state)
+    {
+        Scope globalScope;
+        ast->evaluate(globalScope);
+    }
+}
+
+BENCHMARK(BenchmarkParsingCountTo1M)->Unit(benchmark::kMillisecond);
+BENCHMARK(BenchmarkEvaluatingCountTo1M)->Unit(benchmark::kMillisecond);
---
+++ test/functional/InterpreterTests.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file InterpreterTests.cpp
+ * @author Edward Palmer
+ * @date 2025-04-16
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "EucleiaInterpreter.hpp"
+#include "Scope.hpp"
+#include "test/utility/Utility.hpp"
+#include <cstdlib>
+#include <gtest/gtest.h>
+
+
+std::string testDataPath(std::string fileName);
+
+
+TEST(InterpreterTestSuite, ArrayTests)
+{
+    Interpreter::evaluateFile(testDataPath("ArrayTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, BoolTests)
+{
+    Interpreter::evaluateFile(testDataPath("BoolTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, ClassTests)
+{
+    Interpreter::evaluateFile(testDataPath("ClassTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, InheritanceTests)
+{
+    Interpreter::evaluateFile(testDataPath("InheritanceTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, IntTests)
+{
+    Interpreter::evaluateFile(testDataPath("IntTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, LoopTests)
+{
+    Interpreter::evaluateFile(testDataPath("LoopTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, MiscTests)
+{
+    Interpreter::evaluateFile(testDataPath("MiscTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, ReferenceTests)
+{
+    Interpreter::evaluateFile(testDataPath("ReferenceTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, ScopeTests)
+{
+    Interpreter::evaluateFile(testDataPath("ScopeTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, StringTests)
+{
+    Interpreter::evaluateFile(testDataPath("StringTests.ek"));
+}
+
+
+TEST(InterpreterTestSuite, StructTests)
+{
+    Interpreter::evaluateFile(testDataPath("StructTests.ek"));
+}
+
+
+std::string testDataPath(std::string fileName)
+{
+    return getTestDirPath() + "functional/data/" + fileName;
+}
